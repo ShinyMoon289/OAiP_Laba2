647017ac68ecdae41b7b6a1ae973de8f565bed5f
ShinyMoon289 <76493639+ShinyMoon289@users.noreply.github.com>
2022-09-28 18:23:03 +0300

    Add files via upload

---
+++ Na9(2)/ZadanieNa9(2).cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main()
+{
+	float a, b, c = 0;
+	int d = 0;
+	printf("Введите три числа\n");
+	scanf_s("%f%f%f", &a,&b,&c);
+	if (a - round(a) == 0) d++;
+	if (b - round(b) == 0) d++;
+	if (c - round(c) == 0) d++;
+
+	printf("Целых чисел -- %d", d);
+	return 0;
+
+
+
+	
+}
